2bf3238a1c3b2033fb053a520fac05037f36a649
Chris Schniepp <160089891+ChrisSchniepp@users.noreply.github.com>
2024-12-13 15:12:32 -0800

    Doxygen Files

---
+++ Doxygen/html/annotated_dup.js
@@ -0,0 +1,27 @@
+var annotated_dup =
+[
+    [ "encoder", null, [
+      [ "Encoder", "classencoder_1_1_encoder.html", "classencoder_1_1_encoder" ]
+    ] ],
+    [ "final", null, [
+      [ "PIDController", "classfinal_1_1_p_i_d_controller.html", null ]
+    ] ],
+    [ "imu", null, [
+      [ "IMU", "classimu_1_1_i_m_u.html", "classimu_1_1_i_m_u" ]
+    ] ],
+    [ "motor", null, [
+      [ "Motor", "classmotor_1_1_motor.html", "classmotor_1_1_motor" ]
+    ] ],
+    [ "new_dir", null, [
+      [ "PIDController", "classnew__dir_1_1_p_i_d_controller.html", null ]
+    ] ],
+    [ "qtrx", null, [
+      [ "QTRX", "classqtrx_1_1_q_t_r_x.html", "classqtrx_1_1_q_t_r_x" ]
+    ] ],
+    [ "test", null, [
+      [ "PIDController", "classtest_1_1_p_i_d_controller.html", null ]
+    ] ],
+    [ "test_2", null, [
+      [ "PIDController", "classtest__2_1_1_p_i_d_controller.html", null ]
+    ] ]
+];
